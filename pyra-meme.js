65cb55a1779be2bd6d17faff255dfd743dfea400
Kyle J. Kemp <kyle@seiyria.com>
2017-12-26 11:03:27 -0600

    Updating pyra-meme.js

--- pyra-meme.js
+++ pyra-meme.js
@@ -1,7 +1,19 @@
 const sample = require('lodash.sample');
 
+const allMemes = [
+  'THINK YOU CAN TAKE ME?',
+  'I\'LL SHOW YOU A THING OR THREE!',
+  'DON\'T FORGET ME!',
+  'I\'m really feeling it!',
+  'We\'ll beat them with the powre of friendship!',
+  'Ready or Gormotti!',
+  'MEH-MEH-MEH-MEH-MEH!',
+  'Oh, and I\'ve started glowing. I\'m a bit self-conscious...',
+  'It\'s blushy-crushy time!'
+];
+
 module.exports = function(context, cb) {
   cb(null, [
-    { message: 'THINK YOU CAN TAKE ME?' }  
+    { message: sample(allMemes) }  
   ]);
 };
